0723ce5642412ddc85a86b9732ee628ffde5eaea
RickN123 <nicolai.richard@gmail.com>
2019-03-28 17:13:52 -0400

    game layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+    var phillynames = "eagles, rocky, wentz, flyers"
+
+    function teamletters(letter) {
+
+        for (var i = 0; i <phillynames.length; i++) {
+            if (phillynames [i] ==letter) {
+        
+            }
+        }
+
+    }
+
+
+
