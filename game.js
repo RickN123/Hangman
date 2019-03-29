8dbcf0774c51d2070897befe2e1d0c0b0ac74b73
RickN123 <nicolai.richard@gmail.com>
2019-03-28 23:01:11 -0400

    New scoring

--- game.js
+++ game.js
@@ -1,27 +1,72 @@
 
 
 
-document.onkeyup = function () 
-    
-    var Wins = 0;
-    var Losses = 0;
-    var guessesLeft = 0;
-    var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    var GuessesDone = 0;
-    var correctguess= null;
 
 
-document.onkeypress = function(event)
+var wins = 0;
+var losses = 0;
+var guessesLeft = 0;
+var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var guessesDone = 0;
+var correctGuess = null;
+
+function initialize() {
+
+    wins = 0;
+    losses = 0;
+    guessesLeft = 10;
+    guessesDone = 0;
+    correctGuess = null;
+    updateCountsOnUI();
+}
+
+
+document.onkeyup = function (event) {
+    if (guessesDone >= 10) {
+        return;
+    }
+    var userGuess = event.key;
+    var computerGuess = alphabets[Math.floor(Math.random() * alphabets.length)];
+
+    console.log("Key up detected", userGuess, computerGuess);
+
+    if (userGuess === computerGuess) {
+        wins += 1;
+    } else {
+        losses += 1;
+    }
+    guessesLeft -= 1;
+    guessesDone += 1;
+
+
+    updateCountsOnUI();
+
+
+
+}
+
+function updateCountsOnUI() {
+
+    document.getElementById('Wins').innerHTML = "Wins: " + wins;
+    document.getElementById('Losses').innerHTML = "Losses: " + losses;
+    document.getElementById('GuessesLeft').innerHTML = "GuessesLeft: " + guessesLeft
+    document.getElementById('GuessesDone').innerHTML = "GuessesDone: " + guessesDone
+}
+
+initialize();
+/*
+
+document.onkeyup = function(event)
     var computerguess = alphabet [(Math.floor(math.random()* alphabet.length)];
 
     var userguess = String.fromCharCode(event.keyCode).toLowerCase();
 
-    if (userguess === correctguess) {getElementById"countWins"(add1);
+    if (userguess === correctguess) document.getElementById"countWins"(add1);
 
     } else {
         guessesLeft --;
     }
-    
+
     if (guessesLeft == 0) {
         loss ();
     }
@@ -45,20 +90,20 @@ document.onkeypress = function(event)
     document.getElementById('Losses').innerHTML= "Losses: " + losses;
     document.getElementById('guessesLeft').innerHTML = "GuessesLeft: " + guessesLeft
     document.getElementById('guessesDone').innerHTML = "GuessesDone: " + GuessesDone
-    
+
     DocumentType userguess= ;
     )
     document.onkeypress = function(event) {
         var userkeyCode).toUpperCase();
     var enter = (event.keyCode);
-    var fromCharCode = add letterguess; 
+    var fromCharCode = add letterguess;
     var keyCode= add letter;
 }
-        
+
 
             }
-            
-            
+
+
 
             }
         }
@@ -67,3 +112,4 @@ document.onkeypress = function(event)
 
 
 
+*/
