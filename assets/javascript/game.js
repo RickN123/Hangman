0dfffa5c8c659e182c9218d7d741aff19949f1dc
RickN123 <nicolai.richard@gmail.com>
2019-03-28 21:31:46 -0400

    new loss count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 
 function onkeyup
     
-    var Wins = "eagles"
+    var Wins = 0;
     var Losses = 0;
     var guessesLeft = 0;
     var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -14,9 +14,39 @@ function onkeyup
 document.onkeypress = function(event)
     var computerguess = alphabet [(Math.floor(math.random()* alphabet.length)];
 
-    var userguess = String.fromCharCode(event.keyCode)
+    var userguess = String.fromCharCode(event.keyCode);
 
+    if (userguess === correctguess) {win();
+
+    } else {
+        guessesLeft --;
+    }
     
+    if (guessesLeft == 0) {
+        loss ();
+    }
+
+    function win() {
+        wins++;
+        reset();
+
+    }
+
+    function loss () {
+        lossess++;
+        reset();
+    }
+
+    function reset () {
+        answer = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+
+    document.getElementById('Wins').innerHTML= "Wins: " + wins;
+    document.getElementById('Losses').innerHTML= "Losses: " + losses;
+    document.getElementById('guessesLeft').innerHTML = "GuessesLeft: " + guessesLeft
+    document.getElementById('guessesDone').innerHTML = "GuessesDone: "
+
+    )
     document.onkeypress = function(event) {
         var userkeyCode).toUpperCase();
     var enter = (event.keyCode);
